a040923ac6b67de94d71f38b3e7450acbe396779
Evan <hayesevan@rocketmail.com>
2019-01-17 14:19:12 -0500

    Initial commit

---
+++ Springit2Application.java
@@ -0,0 +1,14 @@
+package com.ehayes.springit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springit2Application.class, args);
+	}
+
+}
+
