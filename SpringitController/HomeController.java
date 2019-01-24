8d6e565f2d3267d44e7dc7194312326919831119
Evan <hayesevan@rocketmail.com>
2019-01-24 10:43:45 -0500

    Started the Controller part of MVC

---
+++ SpringitController/HomeController.java
@@ -0,0 +1,14 @@
+package com.ehayes.springit2.SpringitController;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
